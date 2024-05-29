7125617ac823ce8db3f194dad13d316d955193db
phamthanhdat-hub <thanhdat160924@gmail.com>
2024-05-29 12:55:51 +0700

    ls6

---
+++ PTD-lesson06/Controllers/PtdSongController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTD_lesson06.Models;
+
+namespace PTD_lesson06.Controllers
+{
+    public class PtdSongController : Controller
+    {
+
+        private static List<PtdSong> ptdSongs = new List<PtdSong>()
+            {
+                new PtdSong{Id=2210900007,PtdTitle="Pham Thanh Dat",PtdAuthor="k22CNT3",Ptdartist="NTU",PtdYearRelease=2020},
+                  new PtdSong{Id=1,PtdTitle="Pham Thanh Dat",PtdAuthor="k22CNT3",Ptdartist="NTU",PtdYearRelease=2020},
+            };
+
+        //Get: PtdSong
+        /// <summary>
+        ///  <summary>
+        ///  Hien thi danh sach bai hat
+        ///  Author:Pham Thanh Dat
+        ///  </summary>
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PtdIndex()
+        {
+            return View();
+        }
+        //Get: PtdCreate
+        /// <summary>
+        /// Form Them moi bai hat
+        /// Author: Pham Thanh Dat
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PtdCreate()
+        {
+            var PtdSong = new PtdSong();
+            return View();
+        }
+    }
+}
---
+++ PTD-lesson06/Models/PtdSong.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace PTD_lesson06.Models
+{
+    public class PtdSong
+    {
+        [Key]
+        public double Id { get; set; }
+        [Required(ErrorMessage ="Ptd: Hay nhap ten tieu de")]
+        [DisplayName("Tieu de")]
+        public string PtdTitle { get; set; }
+        [Required(ErrorMessage = "Ptd: Hay nhap ten tac gia")]
+        [DisplayName("Tac gia")]
+        public string PtdAuthor { get; set; }
+        [Required(ErrorMessage = "Ptd: Hay nhap nghe si")]
+        [StringLength(50,MinimumLength =2,ErrorMessage ="Ptd: Ten nghe si co toi thieu 2 ky tu,toi da 50 ky tu")]
+        [DisplayName("Nghe si")]
+        public string Ptdartist { get; set; }
+        [Required(ErrorMessage = "Ptd: Hay nhap nam xuat ban")]
+        [RegularExpression(@"[0,9]{4}",ErrorMessage ="Ptd: Nha xuat ban phai co 2 ky tu so")]
+        [Range(1900,2024,ErrorMessage ="Ptd: Nam xuat ban trong khoang 1900-2024")]
+        [DisplayName("Nam xuat ban")]
+        public int PtdYearRelease { get; set; }
+
+
+    }
+}
